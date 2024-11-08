4b5ca16f7a5cb30f4fc7f6a81c307ed75bb028ce
Athul-Ramesan <athulrameshankv.ar@gmail.com>
2024-11-08 16:00:48 +0530

    feat: added inital setup for todo (#1)

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const todo = ()=>{
+    const makeToDoheading = document.getElementById("make-todo-head")
+    makeToDoheading.style.color = "red"
+}
+
+
+document.addEventListener('DOMContentLoaded',todo)
