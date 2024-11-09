bbb29d5013502b9bfd9108b0d21d1a69d512c6ad
Athul-Ramesan <135419747+Athul-Ramesan@users.noreply.github.com>
2024-11-09 16:54:32 +0530

    fix: updated code and cleanedup (#12)

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const todo = ()=>{
     const addTaskInput = document.getElementById("add-todo-input")
     const dueDateInput = document.getElementById("due-date")
     const todoForm = document.getElementById("todo-form")
+    const today = new Date().toISOString().split('T')[0]
+    const dueDate = document.getElementById('due-date');
+    dueDate.setAttribute('min', today);
     displayTasks()
     const handleAddTask = (e)=>{
         e.preventDefault()
